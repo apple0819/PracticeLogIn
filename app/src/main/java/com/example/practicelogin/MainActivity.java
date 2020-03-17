862a447318aaf46503b668cd81c23edb4a005557
apple0819 <apple0819@users.noreply.github.com>
2020-03-17 12:05:39 +0900

    버튼을 JAVA에서 변경

--- app/src/main/java/com/example/practicelogin/MainActivity.java
+++ app/src/main/java/com/example/practicelogin/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.practicelogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn = null;
+    TextView findPwTxt = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        findPwTxt = findViewById(R.id.findPwTxt);
+
+//        로그인버튼의 글자를 => 회원가입으로 변경.
+        loginBtn.setText("회원가입");
+
+//        비밀번호 찾기 텍스트뷰를 => JAVA에서 "비번 찾기"로 변경.
+        findPwTxt.setText("비번 찾기");
+
     }
 }
